0bf1b29c4ed3849ebb947df31b97765012365391
Klaudia Wolska <klaudia@MacBook-Air-Klaudia.local>
2023-05-30 22:16:25 +0200

    poprawiono błędne nazewnictwo metod klasy kolo i dodano dekorator @abstractmethod

---
+++ 10. Dziedziczenie/main.py
@@ -0,0 +1,19 @@
+from klasa import Kwadrat, Prostokat, Trojkat, Kolo
+import math 
+
+kwadrat = Kwadrat(3)
+print("Pole kwadratu: ", kwadrat.pole())
+print("Obwód kwadratu: ", kwadrat.obwod())
+
+prostokat = Prostokat(2,5)
+print("Pole prostokątu: ", prostokat.pole())
+print("Obwód prostokątu: ", prostokat.obwod())
+
+trojkat = Trojkat(3,4,5)
+print("Pole trójkątu: ", trojkat.pole())
+print("Obwód trójkątu: ", trojkat.obwod())
+
+kolo = Kolo(2)
+print("Pole koła: ", kolo.pole())
+print("Obwód koła: ", kolo.obwod())
+
