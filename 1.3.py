e7045a1feb377d2867fd7628bea5e471d02e83b3
Klaudia Wolska <klaudia@MacBook-Air-Klaudia.local>
2023-05-30 18:34:11 +0200

    poprawiono działanie programu i dodano wyświetlanie liczby prób uytkownika

---
+++ 1.3.py
@@ -0,0 +1,24 @@
+import random
+
+while True:
+    liczba = (random.randint(0, 101))
+    proby = 0
+
+    while True:
+        zgadnieta = int(input("zgadnięta liczba: "))
+        proby += 1
+
+        if zgadnieta > liczba:
+            print("wylosowana liczba jest mniejsza")
+        elif zgadnieta < liczba:
+            print("wylosowana liczba jest większa")
+        else:
+            print("zgadłeś wylosowaną liczbę!")
+            print("zgadłeś po:" ,proby , "próbach")
+            print("czy grasz jeszcze?")
+            odp = input("wpisz 'tak' lub 'nie': ")
+            if odp == "tak":
+                break
+            elif odp == "nie":
+                exit()
+            
