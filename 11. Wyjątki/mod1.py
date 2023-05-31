447e5079db16d2fbe7158d8ed5eeff96e70f581d
Klaudia Wolska <klaudia@MacBook-Air-Klaudia.local>
2023-05-31 08:29:04 +0200

    dodano nowe wyjątki

---
+++ 11. Wyjątki/mod1.py
@@ -0,0 +1,15 @@
+def dodawanie(a: float,b: float) -> float:
+    """wykonuje dodawanie"""
+    return a + b
+
+def odejmowanie(a: float,b:float) -> float:
+    """wykonuje odejmowanie"""
+    return a - b
+
+def mnozenie(a:float,b:float) -> float:
+    """wykonuje mnozenie"""
+    return a*b
+
+def dzielenie(a:float,b:float) -> float:
+    """wykonuje dzielenie"""
+    return a / b
